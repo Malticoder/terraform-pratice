2a89a90b0ae8e25be66c51f802d0b1bd891e2724
root <root@ip-172-31-54-247.ec2.internal>
2026-01-19 12:43:59 +0000

    add new files

---
+++ ec2-instance.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "test" {
+  ami = "ami-07ff62358b87c7116"
+  instance_type = "t2.micro"
+}
+
+
+
---
+++ s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "new-bucket" {
+	bucket = "first-bucket-terraform-1"
+}
